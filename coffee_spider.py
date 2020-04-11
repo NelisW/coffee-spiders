e4610f82e559f8606abf6f410fe1aa051cb58629
Jack Turner <jackwilliamturner@icloud.com>
2020-04-11 16:26:10 +0100

    Callable function

--- coffee_spider.py
+++ coffee_spider.py
@@ -9,20 +9,71 @@ from matplotlib.collections import PatchCollection
 plt.rcParams['font.family'] = 'sans-serif'
 plt.rcParams['font.weight']  = 'bold'
 
-## draw N length lines from center (can have 5 points on them with marker and line style)
-fig, ax = plt.subplots(figsize=(8,8))
-
 def pol2cart(rho, phi):
     x = rho * np.cos(phi)
     y = rho * np.sin(phi)
     return(x, y)
 
-#### setup
-num_points = 14
-angle_step = (2*np.pi) / num_points
+def spider_plot(scores, ax, max_score=.5, markers=5):
+    #### setup
+    num_points = len(scores)
+    angle_step = (2*np.pi) / num_points
 
-max_score = .5
-markers   = 5
+    ## draw background grid
+    marker_style = dict(color='gray', linestyle=':', marker='o',
+                        markersize=5, markerfacecolor='white')
+
+    labels = list(scores.keys())
+
+    for i in range(0, num_points):
+        pts = np.linspace(0,max_score, markers)
+        theta = -(angle_step*i)
+
+        xs = []
+        ys = []
+        for p in pts:
+            x,y = pol2cart(p, theta)
+            xs.append(x)
+            ys.append(y)
+
+        ax.plot(xs, ys, zorder=0, **marker_style)
+
+        rot = (theta * (180/np.pi))
+        va, ha = 'center', 'left'
+        label = labels[i]
+        if (rot < -90.) and (rot > -270):
+            rot += 180
+            ha = 'right'
+            label += '  '
+        else:
+            label = '  ' + label
+
+        ax.annotate(label, (x,y), rotation_mode='anchor', rotation=rot, verticalalignment=va, horizontalalignment=ha)
+
+    ## draw polygons
+    patches = []
+
+    values = list(scores.values())
+    values.append(values[0]) # so that we can wrap around at the end
+
+    # colors
+    cmap = matplotlib.cm.get_cmap('plasma',14)
+
+    for i in range(0, num_points):
+        pts = [(0,0), pol2cart(values[i],-(angle_step*i)), pol2cart(values[i+1],-(angle_step*(i+1)))]
+        poly = Polygon(pts, closed=False, zorder=10)
+        patches.append(poly)
+
+    p = PatchCollection(patches, cmap=cmap)
+
+    p.set_array(np.linspace(0,1,num_points-1))
+    ax.add_collection(p)
+
+    ax.set_xlim(-max_score-.1,max_score+.1)
+    ax.set_ylim(-max_score-.1,max_score+.1)
+
+    return ax
+
 
 scores = OrderedDict([
     ('FLORAL', .1),
@@ -41,59 +92,10 @@ scores = OrderedDict([
     ('ACIDITY', .4),
 ])
 
+## draw N length lines from center (can have 5 points on them with marker and line style)
+fig, ax = plt.subplots(figsize=(8,8))
 
-## draw background grid
-marker_style = dict(color='gray', linestyle=':', marker='o',
-                    markersize=5, markerfacecolor='white')
-
-labels = list(scores.keys())
-
-for i in range(0, num_points):
-    pts = np.linspace(0,max_score, markers)
-    theta = -(angle_step*i)
-
-    xs = []
-    ys = []
-    for p in pts:
-        x,y = pol2cart(p, theta)
-        xs.append(x)
-        ys.append(y)
-
-    ax.plot(xs, ys, zorder=0, **marker_style)
-
-    rot = (theta * (180/np.pi))
-    va, ha = 'center', 'left'
-    label = labels[i]
-    if (rot < -90.) and (rot > -270):
-        rot += 180
-        ha = 'right'
-        label += '  '
-    else:
-        label = '  ' + label
-
-    ax.annotate(label, (x,y), rotation_mode='anchor', rotation=rot, verticalalignment=va, horizontalalignment=ha)
-
-## draw polygons
-patches = []
-
-values = list(scores.values())
-values.append(values[0]) # so that we can wrap around at the end
-
-# colors
-cmap = matplotlib.cm.get_cmap('plasma',14)
-
-for i in range(0, num_points):
-    pts = [(0,0), pol2cart(values[i],-(angle_step*i)), pol2cart(values[i+1],-(angle_step*(i+1)))]
-    poly = Polygon(pts, closed=False, zorder=10)
-    patches.append(poly)
-
-p = PatchCollection(patches, cmap=cmap)
-
-p.set_array(np.linspace(0,1,num_points-1))
-ax.add_collection(p)
-
-ax.set_xlim(-max_score-.1,max_score+.1)
-ax.set_ylim(-max_score-.1,max_score+.1)
+ax = spider_plot(scores, ax)
 
 ## remove axis labels
 plt.axis('off')
